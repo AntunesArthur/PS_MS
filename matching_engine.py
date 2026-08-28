cd7682222236b57446e80b09c4496a418217919c
Arthur <arthurhenriqque21@gmail.com>
2026-08-27 23:16:39 -0300

    implement place_limit_order with FIFO queue per price level

--- matching_engine.py
+++ matching_engine.py
@@ -2,6 +2,9 @@ from sortedcontainers import SortedDict
 from collections import deque
 from decimal import Decimal
 from itertools import count
+import uuid 
+
+_sequence_counter = count()
 
 def create_book():
     #Retorna apenas a estrutura vazia do book, que utilizaremos para auxiliar nos trades
@@ -10,4 +13,23 @@ def create_book():
         'sell': SortedDict(),
         'orders_by_id': {},
     }
-    
+
+def place_limit_order(book, side, price, qty):
+    book_order = {
+        'id': str(uuid.uuid4()),
+                'side': side,
+                'order_type': 'limit',
+                'price': price,
+                'qty': qty,
+                'remaining_qty': qty,
+                'sequence': next(_sequence_counter)
+    }
+    if price not in book[side]:
+        book[side][price] = deque()
+    book[side][price].append(book_order)
+
+    book['orders_by_id'][book_order['id']] = book_order
+
+    return book_order
+
+
